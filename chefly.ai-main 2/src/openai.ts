7c78edfac16472bb4afa6dbf772a8b98a1ff1096
David Park <parkd947@newschool.edu>
2023-12-03 21:17:49 -0500

    newly added api key
    updated to recieved new api key

--- chefly.ai-main 2/src/openai.ts
+++ chefly.ai-main 2/src/openai.ts
@@ -1,7 +1,7 @@
 import OpenAI from "openai";
 
 const openAI = new OpenAI ({
- apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+ apiKey: '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',
  //@ts-expect-error
  model: "gpt-3.5-turbo",
  dangerouslyAllowBrowser: true
